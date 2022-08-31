c53fe6c150efde8b207b0e6d465234eda475ec83
Laurent De Marez <laurent@demarez.org>
2022-08-31 13:56:31 +0200

    feat: display all vesting stakeholders in table

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { REQUEST_VESTING_ABI, REQUEST_VESTING_CONTRACT } from "@/lib/constants"
+import { ERC20_SYMBOLS, REQUEST_VESTING_ABI, REQUEST_VESTING_CONTRACT } from "@/lib/constants"
 import { Contract, providers } from "ethers"
 import { formatEther } from "ethers/lib/utils"
 import { useEffect, useState } from "react"
@@ -6,6 +6,68 @@ import { useEffect, useState } from "react"
 const provider = new providers.AlchemyProvider(1, "YEYRkmN24MyBT8cNXvEEL_LzrWHMBstJ")
 const vestingContract = new Contract(REQUEST_VESTING_CONTRACT, REQUEST_VESTING_ABI, provider)
 
+const getTokenSymbol = (tokenAddress) => ERC20_SYMBOLS?.[tokenAddress]
+
+const VestingTable = ({ grants }) => {
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <h1 className="text-xl font-semibold text-gray-900">Request Network Vesting Contract</h1>
+      <div className="mt-8 flex flex-col">
+        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                      Stakeholder
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      Allocation
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      % Vested
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      Start
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      Cliff
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      End
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {grants.map((grant, idx) => {
+                    const formatter = new Intl.NumberFormat('en-US', {
+                      style: 'currency',
+                      currency: getTokenSymbol(grant.tokenAddress)
+                    })
+                    return (
+                      <tr key={idx} className={grant.blockRevoked && "bg-red-100"}>
+                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                          {grant.vester}
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{formatter.format(grant.amount)}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500"></td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{grant.start}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{grant.end}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{grant.cliff}</td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+}
 
 export default function Home() {
   const [grants, setGrants] = useState([])
@@ -44,7 +106,7 @@ export default function Home() {
 
   return (
     <>
-
+      <VestingTable grants={grants} />
     </>
   )
 }
