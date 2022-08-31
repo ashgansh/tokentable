98a4fe6100b86de8da1519fc8b3f170b2ba8ae88
Laurent De Marez <laurent@demarez.org>
2022-08-31 13:33:50 +0200

    feat: ability to know when vesting is revoked

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,50 @@
 import { REQUEST_VESTING_ABI, REQUEST_VESTING_CONTRACT } from "@/lib/constants"
 import { Contract, providers } from "ethers"
 import { formatEther } from "ethers/lib/utils"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const provider = new providers.AlchemyProvider(1, "YEYRkmN24MyBT8cNXvEEL_LzrWHMBstJ")
 const vestingContract = new Contract(REQUEST_VESTING_CONTRACT, REQUEST_VESTING_ABI, provider)
 
 
 export default function Home() {
+  const [grants, setGrants] = useState([])
   useEffect(() => {
     const retrieveNewGrantEvents = async () => {
       const newGrants = await vestingContract.queryFilter("NewGrant")
       const revokedGrants = await vestingContract.queryFilter("GrantRevoked")
       const grants = newGrants.map(log => {
-        //const blockRevoked = revokedGrants
-        //  .filter(revokedGrant => )
+        const { token: tokenAddress, vester, granter } = log.args
+        const blockRevoked = revokedGrants
+          .filter(revokedGrant => (
+            granter === revokedGrant.args?.granter &&
+            vester === revokedGrant.args?.vester &&
+            tokenAddress === revokedGrant.args?.token
+          ))
+          .shift()
+          ?.blockNumber
         const grant = {
-          granter: log.args?.granter,
-          vester: log.args?.vester,
-          tokenAddress: log.args?.token,
+          granter,
+          vester,
+          tokenAddress,
           amount: formatEther(log.args?.vestedAmount),
           start: log.args?.startTime?.toNumber(),
           end: log.args?.endTime?.toNumber(),
           cliff: log.args?.cliffTime?.toNumber(),
           blockCreated: log.blockNumber,
-          blockRevoked: null
+          blockRevoked: blockRevoked
         }
         return grant
       })
-    console.log(grants)
-    console.log(revokedGrants)
+      console.log(grants)
+      setGrants(grants)
     }
-
     retrieveNewGrantEvents()
   }, [])
+
   return (
     <>
+
     </>
   )
 }
