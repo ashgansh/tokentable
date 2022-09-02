10f669b731da046215e7916aa4f7f92e9fc033e0
Laurent De Marez <laurent@demarez.org>
2022-09-02 14:42:29 +0200

    chore: move vesting dashboards to one page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,67 @@
+import VestingInsights from "@/components/VestingInsights"
+import VestingTable from "@/components/VestingTable"
+import { getVestingData as getRequestVestingData } from "@/lib/indexer/RequestNetwork"
+import { getVestingData as getZoraclesVestingData } from "@/lib/indexer/Zoracles"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+
+const VestingDashboard = ({ contractType, contractAddress, chainId }) => {
+  const [vestingData, setVestingData] = useState()
+
+  useEffect(() => {
+    if (!contractType || !contractAddress || !chainId) return
+
+    const retrieveVestingData = async () => {
+      if (contractType === 'request') {
+        const vestingData = await getRequestVestingData(chainId, contractAddress)
+        setVestingData(vestingData)
+        return
+      }
+      if (contractType === 'zoracles') {
+        const vestingData = await getZoraclesVestingData(chainId, contractAddress)
+        setVestingData(vestingData)
+        return
+      }
+    }
+
+    retrieveVestingData()
+  }, [contractType, contractAddress, chainId])
+
+  if (!vestingData) return <>Loading</>
+
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <h1 className="text-xl font-semibold text-gray-900">{contractType} - {contractAddress}</h1>
+      {Object.keys(vestingData.tokens).map(tokenAddress => (
+        <>
+          <VestingInsights
+            totalAllocated={vestingData.totalAllocatedAmounts?.[tokenAddress] || BigNumber.from(0)}
+            totalWithdrawn={vestingData.totalWithdrawnAmounts?.[tokenAddress] || BigNumber.from(0)}
+            totalVested={vestingData.totalVestedAmounts?.[tokenAddress] || BigNumber.from(0)}
+            tokenAddress={tokenAddress}
+            tokens={vestingData.tokens} />
+          <VestingTable grants={vestingData.grants} tokens={vestingData.tokens} />
+        </>
+      ))}
+    </div>
+  )
+}
+
 const Home = () => {
-  return <></>
+  const { query } = useRouter()
+  const { contractType, contractAddress } = query
+
+  if (!contractAddress || !contractType) {
+    return (
+      <ul>
+        <li><Link href="/?contractType=request&contractAddress=0x45E6fF0885ebf5d616e460d14855455D92d6CC04">Request Network</Link></li>
+        <li><Link href="/?contractType=zoracles&contractAddress=0x2369921551f2417d8d5cD4C1EDb1ac7eEe156380">Zoracles</Link></li>
+      </ul>
+    )
+  }
+
+  return <VestingDashboard contractType={contractType} contractAddress={contractAddress} chainId={1} />
 }
 
 export default Home
