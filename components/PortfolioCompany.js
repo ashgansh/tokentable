7928d7b9e2ed49fa4ec15417f064819208370737
Laurent De Marez <laurent@demarez.org>
2022-09-05 12:55:59 +0200

    feat: add portfolio components

---
+++ components/PortfolioCompany.js
@@ -0,0 +1,72 @@
+import Moment from "react-moment"
+
+const PortfolioCompany = ({ companyLogo, vestingStartTime, vestingEndTime, vestingCliffTime, allocationUSD, allocationToken, marketCapCurrent, marketCapTGE, circulatingSupply }) => {
+  const ItemTitle = ({ children }) => <h4 className="text-sm text-bold text-gray-900 py-2.5">{children}</h4>
+
+  const now = Date.now() / 1000
+  const nowOrVestingEnd = Math.min(now, vestingEndTime)
+  const vestingPercentage = Math.round(((nowOrVestingEnd - vestingStartTime) / (vestingEndTime - vestingStartTime)) * 100)
+  const vestingPercentageFormatted = `${vestingPercentage}%`
+
+  return (
+    <div className="border border-gray-200 shadow rounded-lg px-4 py-4 px-6">
+      <div className="grid grid-rows-2 grid-cols-[repeat(4,minmax(0,1fr))] grid-flow-col gap-x-20 gap-y-4">
+        <div className="">
+          <ItemTitle>Company</ItemTitle>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={companyLogo} alt="Company Logo" className="h-8" />
+        </div>
+        <div>
+          <div className="flex justify-between items-center">
+            <ItemTitle>Vested</ItemTitle>
+            <span className="text-sm text-gray-500 py-2.5">{vestingPercentageFormatted}</span>
+          </div>
+          <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+            <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: vestingPercentageFormatted }}></div>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="text-sm text-gray-500 py-1.5">
+              <Moment unix format="MMM YYYY">{vestingStartTime}</Moment>
+            </span>
+            <span className="text-sm text-gray-500 py-1.5">
+              <Moment unix format="MMM YYYY">{vestingEndTime}</Moment>
+            </span>
+          </div>
+        </div>
+        <div>
+          <ItemTitle>Chain</ItemTitle>
+          <svg className="w-8 h-8 text-[#626890]" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="ethereum" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path></svg>
+        </div>
+        <div>
+          <ItemTitle>Allocation</ItemTitle>
+          <div className="text-bold text-xl">{allocationUSD}</div>
+          <div className="text-sm text-gray-500">{allocationToken}</div>
+        </div>
+        <div>
+          <ItemTitle>Market Cap (current)</ItemTitle>
+          <span className="text-bold text-xl block h-8">{marketCapCurrent}</span>
+        </div>
+        <div>
+          <ItemTitle>Market Cap (TGE)</ItemTitle>
+          <span className="text-bold text-xl block">{marketCapTGE}</span>
+        </div>
+        <div>
+          <ItemTitle>Circulating Supply</ItemTitle>
+          <span className="text-bold text-xl block">{circulatingSupply}</span>
+        </div>
+        <div>
+          <ItemTitle>Cliff</ItemTitle>
+          <span className="text-bold text-xl block">
+            {vestingCliffTime && vestingCliffTime !== vestingStartTime ? (
+              <Moment unix format="YYYY-MM-DD">{vestingCliffTime}</Moment>
+            ) : (
+              <>None</>
+            )}
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PortfolioCompany
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,97 @@
-import { LayoutWrapper } from "@/components/LayoutWrapper"
+import { useState, useEffect } from "react"
 import Link from "next/link"
+
+import { LayoutWrapper } from "@/components/LayoutWrapper"
+import PortfolioCompany from "@/components/PortfolioCompany"
+
+import { getVestingData as getRequestVestingData } from "@/lib/indexer/RequestNetwork"
+import { getVestingData as getZoraclesVestingData } from "@/lib/indexer/Zoracles"
+import { getVestingData as getCurveVestingData } from "@/lib/indexer/Curve"
+
+const PortfolioItem = ({contractType, contractAddress, chainId, beneficiaryAddress}) => {
+  const [vestingData, setVestingData] = useState()
+  const beneficiaryGrants = vestingData?.grants?.filter(grant => grant.beneficiary === beneficiaryAddress) || []
+
+  useEffect(() => {
+    if (!contractType || !contractAddress || !chainId) return
+
+    const retrieveVestingData = async () => {
+      if (contractType === 'request') {
+        const vestingData = await getRequestVestingData(chainId, contractAddress)
+        setVestingData(vestingData)
+        return
+      }
+      if (contractType === 'zoracles') {
+        const vestingData = await getZoraclesVestingData(chainId, contractAddress)
+        setVestingData(vestingData)
+        return
+      }
+      if (contractType === 'curve') {
+        const vestingData = await getCurveVestingData(chainId, contractAddress)
+        setVestingData(vestingData)
+        return
+      }
+    }
+
+    retrieveVestingData()
+  }, [contractType, contractAddress, chainId])
+
+  return beneficiaryGrants.map((grant, index) => {
+    console.log(grant)
+    return (
+        <PortfolioCompany
+          key={index}
+          companyLogo="https://raw.githubusercontent.com/RequestNetwork/Request/master/Hubs/Request%20Logos/OnLight/svg/Request_onlight_reg_green.svg"
+          vestingStartTime={grant.startTime}
+          vestingEndTime={grant.endTime}
+          vestingCliffTime={grant.cliffTime}
+          allocationToken="150M REQ"
+          allocationUSD="$ 15M"
+          marketCapCurrent="$ 500M"
+          marketCapTGE="$ 100M"
+          circulatingSupply="900M"
+        />
+    )
+  })
+}
 
 const Home = () => (
   <LayoutWrapper>
+    <PortfolioItem
+      contractType="request"
+      contractAddress="0x45E6fF0885ebf5d616e460d14855455D92d6CC04"
+      chainId={1}
+      beneficiaryAddress="0xF0068a27c323766B8DAF8720dF20a404Cf447727"
+    />
+    <hr className="py-4"/>
+    <hr className="py-4"/>
     <ul>
+      <li>
+        <PortfolioCompany
+          companyLogo="https://raw.githubusercontent.com/RequestNetwork/Request/master/Hubs/Request%20Logos/OnLight/svg/Request_onlight_reg_green.svg"
+          vestingStartTime={1627768800}
+          vestingEndTime={1690840800}
+          vestingCliffTime={1627768800}
+          allocationToken="150M REQ"
+          allocationUSD="$ 15M"
+          marketCapCurrent="$ 500M"
+          marketCapTGE="$ 100M"
+          circulatingSupply="900M"
+        />
+      </li>
+      <li>
+        <PortfolioCompany
+          companyLogo="https://github.com/curvefi/curve-dao/blob/curvedao/src/assets/curve.png?raw=true"
+          vestingStartTime={1627768800}
+          vestingEndTime={1690840800}
+          vestingCliffTime={1627768800}
+          allocationToken="150M REQ"
+          allocationUSD="$ 15M"
+          marketCapCurrent="$ 500M"
+          marketCapTGE="$ 100M"
+          circulatingSupply="900M"
+        />
+      </li>
       <li><Link href="/vesting/request/0x45E6fF0885ebf5d616e460d14855455D92d6CC04">Request Network</Link></li>
       <li><Link href="/vesting/zoracles/0x2369921551f2417d8d5cD4C1EDb1ac7eEe156380">Zoracles</Link></li>
       <li><Link href="/vesting/curve/0x2a7d59e327759acd5d11a8fb652bf4072d28ac04">Curve</Link></li>
