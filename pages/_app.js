cfc065e98029d042001b600db15caf67cc98ec02
Laurent De Marez <laurent@demarez.org>
2022-09-07 16:37:28 +0200

    chore: use wagmi to manage providers

--- pages/_app.js
+++ pages/_app.js
@@ -6,21 +6,10 @@ import {
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
 import {
-  chain,
-  configureChains,
   createClient,
   WagmiConfig,
 } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-
-const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-  [
-    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-    publicProvider()
-  ]
-);
+import { chains, provider } from "@/lib/provider"
 
 const { connectors } = getDefaultWallets({
   appName: 'Token Ops',
--- pages/index.js
+++ pages/index.js
@@ -85,11 +85,13 @@ const Home = () => {
   return (
     <LayoutWrapper>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-        <h1 className="text-2xl font-semibold text-gray-900">Portfolio</h1>
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold text-gray-900">Portfolio</h1>
+          <Link href="/portfolio-edit">Edit</Link>
+        </div>
       </div>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
         <Portfolio />
-        <Link href="/portfolio-edit">Edit Portfolio</Link>
       </div>
     </LayoutWrapper>
   )
