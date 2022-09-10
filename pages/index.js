7413993214bd45e2f48fea9fa22ca623a1713ca9
Laurent De Marez <laurent@demarez.org>
2022-09-10 14:20:06 +0200

    chore: remove edit button from portfolio

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,6 @@ const Home = () => {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
         <div className="flex justify-between">
           <h1 className="text-2xl font-semibold text-gray-900">Portfolio</h1>
-          <Link href="/portfolio-edit">Edit</Link>
         </div>
       </div>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
