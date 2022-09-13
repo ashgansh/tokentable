3273cb50c706723908edb426557fe04254c554d2
Laurent De Marez <laurent@demarez.org>
2022-09-13 16:05:43 +0200

    feat: ability to add items to the portfolio

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
+import { useForm } from "react-hook-form"
+import { isAddress } from "ethers/lib/utils"
 import { useState, useEffect } from "react"
 import Link from "next/link"
-
-import { LayoutWrapper } from "@/components/LayoutWrapper"
-import PortfolioCompany from "@/components/PortfolioCompany"
+import axios from "axios"
 
 import { formatCurrency, formatAmount } from "@/lib/utils"
 import { getVestingData } from "@/lib/vesting"
 import { useTokenCirculatingSupply, useTokenFormatter, useTokenPrice } from "@/lib/tokens"
 import { portfolioSelector, portfolioStore } from "@/lib/portfolio"
+
+import { PrimaryButton } from "@/components/Button"
+import { Modal, ModalActionFooter, ModalBody, ModalTitle } from "@/components/Modal"
+import { Input, Label } from "@/components/Input"
+import { LayoutWrapper } from "@/components/LayoutWrapper"
+import PortfolioCompany from "@/components/PortfolioCompany"
+import Spinner from "@/components/Spinner"
 
 const PortfolioItem = ({ companyName, companyLogo, startTime, endTime, cliffTime, amount, tokenAddress, chainId }) => {
   const formatToken = useTokenFormatter(chainId, tokenAddress)
@@ -81,12 +88,75 @@ const Portfolio = () => {
   )
 }
 
+const AddContractModal = ({ show, onClose }) => {
+  const { reset, register, handleSubmit, formState: { errors, isSubmitSuccessful, isSubmitting } } = useForm()
+
+  const handleAddContract = async (data) => {
+    await axios.post("https://formspree.io/f/xaykqkok", data)
+    reset()
+  }
+
+  return (
+    <Modal show={show} onClose={onClose}>
+      <form onSubmit={handleSubmit(handleAddContract)}>
+        <ModalTitle>Add a vesting contract to your portfolio</ModalTitle>
+        {isSubmitSuccessful ? (
+          <>
+            <ModalBody>
+              <div className="py-12 items-center flex flex-col gap-4">
+                <span><Spinner className="text-tokenops-primary-700 h-8" /></span>
+                <span>{"We're indexing your contract. This may take up to a 24h."}</span>
+                <span>{"Please check back again later."}</span>
+              </div>
+            </ModalBody>
+            <ModalActionFooter>
+              <PrimaryButton onClick={onClose}>Close</PrimaryButton>
+            </ModalActionFooter>
+          </>
+        ) : (
+          <>
+            <ModalBody>
+              <div className="flex flex-col gap-2.5">
+                <div>
+                  <Label>Contract Address</Label>
+                  <Input {...register("contractAddress", { required: true, validate: { isAddress } })} />
+                  <span className="text-xs text-red-400">
+                    {errors?.contractAddress && "A valid vesting contract address is required"}
+                  </span>
+                </div>
+                <div>
+                  <Label>Beneficiary Address</Label>
+                  <Input {...register("beneficiaryAddress", { required: true, validate: { isAddress }  })} />
+                  <span className="text-xs text-red-400">
+                    {errors?.beneficiaryAddress && "An beneficiary is required"}
+                  </span>
+                </div>
+              </div >
+            </ModalBody >
+            <ModalActionFooter>
+              <PrimaryButton type="submit" disabled={isSubmitting}>Track Contract</PrimaryButton>
+            </ModalActionFooter>
+          </>
+        )}
+      </form >
+    </Modal >
+  )
+
+}
+
 const Home = () => {
+  const [showAddContractModal, setShowAddContractModal] = useState(false)
+
+  const handleOpensAddContractModal = () => setShowAddContractModal(true)
+  const handleCloseAddContractModal = () => setShowAddContractModal(false)
+
   return (
     <LayoutWrapper>
+      <AddContractModal show={showAddContractModal} onClose={handleCloseAddContractModal} />
       <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
         <div className="flex justify-between">
           <h1 className="text-2xl font-semibold text-gray-900">Portfolio</h1>
+          <PrimaryButton onClick={handleOpensAddContractModal}>Add</PrimaryButton>
         </div>
       </div>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
