0d59abcfa7a6cfa8b7808487df17760dda296fe1
Benjamin Shafii <ben@prologe.io>
2022-09-21 15:10:01 +0000

    feat: add link to homepage

--- components/Header.js
+++ components/Header.js
@@ -14,14 +14,16 @@ export default function Header() {
         <Popover className="relative bg-white">
             <div className="flex items-center justify-between px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
                 <div className="flex justify-start lg:w-0 lg:flex-1">
-                    <a href="#">
-                        <span className="sr-only">Your Company</span>
-                        <img
-                            className="h-8 w-auto sm:h-10"
-                            src={TokenOpsLogo.src}
-                            alt=""
-                        />
-                    </a>
+                    <Link href="/">
+                        <a >
+                            <span className="sr-only">Your Company</span>
+                            <img
+                                className="h-8 w-auto sm:h-10"
+                                src={TokenOpsLogo.src}
+                                alt=""
+                            />
+                        </a>
+                    </Link>
                 </div>
                 <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
                     <Link href="/demo">
