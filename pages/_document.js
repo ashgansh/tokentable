0537627ef2b0b61b8f226af3b1e2f07216a298c3
Benjamin Shafii <ben@prologe.io>
2022-10-03 13:54:09 +0000

    analytics: add plausible

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <script
+          defer
+          data-domain="vesting.lol"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
