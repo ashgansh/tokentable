a90ec1e666afbf5b7c050cd975e785e383457777
Benjamin Shafii <ben@prologe.io>
2022-10-12 09:36:32 +0000

    wip: gelato automation

--- pages/test.js
+++ pages/test.js
@@ -44,7 +44,6 @@ import { Contract } from 'ethers';
 
 const CreateGelatoTaskButton = ({
     chainId,
-    operator,
     contractToAutomate,
     sender,
     recipient,
@@ -62,39 +61,33 @@ const CreateGelatoTaskButton = ({
             signer
         );
 
-        const hostInterface = new Interface(HostABI)
+        const hostInterface = new Interface(HostABI);
         const gelatoOps = new GelatoOpsSDK(chainId, signer);
 
-        const callData = constantFlowAgreementContract.interface.encodeFunctionData(
-          'deleteFlowByOperator',
-          //token, sender, receiver, ctx
-          [
-              superTokenContract.address.toLowerCase(),
-              sender.toLowerCase(),
-              recipient.toLowerCase(),
-              []
-          ]
-        )
+        const callData =
+            constantFlowAgreementContract.interface.encodeFunctionData(
+                'deleteFlowByOperator',
+                //token, sender, receiver, ctx
+                [
+                    superTokenContract.address.toLowerCase(),
+                    sender.toLowerCase(),
+                    recipient.toLowerCase(),
+                    [],
+                ]
+            );
 
-        const CFAV1Adress = "0x6EeE6060f715257b970700bc2656De21dEdF074C"
+        const CFAV1Adress = '0x6EeE6060f715257b970700bc2656De21dEdF074C';
 
         const { taskId, tx } = await gelatoOps.createTask({
             execAddress: contractToAutomate,
             execSelector: constantFlowAgreementContract.interface.getSighash(
-                hostInterface.getFunction(
-                    'callAgreement(address,bytes,bytes)'
-                )
+                hostInterface.getFunction('callAgreement(address,bytes,bytes)')
             ),
-            execData:
-                hostInterface.encodeFunctionData(
-                    'callAgreement(address,bytes,bytes)',
-                    //token, sender, receiver, ctx
-                    [
-                        CFAV1Adress,
-                        callData,
-                        "0x"
-                    ]
-                ),
+            execData: hostInterface.encodeFunctionData(
+                'callAgreement(address,bytes,bytes)',
+                //token, sender, receiver, ctx
+                [CFAV1Adress, callData, '0x']
+            ),
             execAbi: hostInterface.format('json'),
             startTime: 120,
             name: 'Stop superfluid stream in 120 seconds',
@@ -104,7 +97,9 @@ const CreateGelatoTaskButton = ({
     const handleGelato = () => {
         createGelatoTask();
     };
-    return <button onClick={handleGelato}>create gelato task</button>;
+    return (
+        <PrimaryButton onClick={handleGelato}>create gelato task</PrimaryButton>
+    );
 };
 
 const UpdateSream = ({ chainId, operator }) => {
@@ -139,46 +134,48 @@ const DeleteStream = ({ chainId, recipient, sender }) => {
         console.log(result);
     };
 
-    return <button onClick={handleDelete}>delte</button>;
+    return <PrimaryButton onClick={handleDelete}>delte</PrimaryButton>;
+};
+
+const GelatoAutomation = ({ sender, recipient }) => {
+    const GELATO_CONTRACT_ADDRESS =
+        '0x527a819db1eb0e34426297b03bae11F2f8B3A19E';
+    const SUPERFLUID_CONTRACT_ADDRESS =
+        '0x3E14dC1b13c488a8d5D310918780c983bD5982E7';
+
+    return (
+        <div className="flex flex-col gap-3">
+            <UpdateSream chainId={137} operator={GELATO_CONTRACT_ADDRESS} />
+            <CreateGelatoTaskButton
+                chainId={137}
+                operator={SUPERFLUID_CONTRACT_ADDRESS}
+                contractToAutomate={SUPERFLUID_CONTRACT_ADDRESS}
+                sender={sender}
+                recipient={recipient}
+            />
+        </div>
+    );
+};
+
+const GelatoAutomationModal = ({ show, onClose, chainId }) => {
+    return (
+        <Modal show onClose={() => null}>
+            <GelatoAutomation />
+        </Modal>
+    );
 };
 
 const Test = () => {
+    console.log('yo');
+
     const { isConnected } = useAccount();
     const hasHydrated = useHasHydrated();
-    // account 5
-    const sender = '0x54a275FB2aC2391890c2E8471C39d85278C23cEe';
-    // account 1
-    const recipient = '0x69F5Bd7021858C3270A43aD7D719c6164CA6D174';
-
-    const useGelato = true;
-    const operator = useGelato
-        ? // gelato contract
-          '0x527a819db1eb0e34426297b03bae11F2f8B3A19E'
-        : // account 3
-          '0x4ee04BfC70DAAA8969f86634Ce8956Cf4014A0CD';
-
-    const contractToAutomate = '0x3E14dC1b13c488a8d5D310918780c983bD5982E7';
     if (!hasHydrated) return <></>;
     if (!isConnected) return <></>;
-    console.log('yo');
+
     return (
         <LayoutWrapper>
-            <div className="flex flex-col gap-3">
-                <UpdateSream chainId={137} operator={operator} />
-                <DeleteStream
-                    chainId={137}
-                    sender={sender}
-                    recipient={recipient}
-                />
-                <CreateGelatoTaskButton
-                    chainId={137}
-                    operator={operator}
-                    contractToAutomate={contractToAutomate}
-                    sender={sender}
-                    recipient={recipient}
-                />
-                {/* <DeleteSuperfluidStreamButton /> */}
-            </div>
+            <GelatoAutomationModal />
         </LayoutWrapper>
     );
 };
