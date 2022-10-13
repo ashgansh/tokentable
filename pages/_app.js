a196a22650d459b4fff61c82ea590adc9bea9d0c
Laurent De Marez <laurent@demarez.org>
2022-10-13 18:26:28 +0200

    feat: add SIWE authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,36 @@
-import '../styles/globals.css'
-import '@rainbow-me/rainbowkit/styles.css';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 
-import { lightTheme, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { WagmiConfig } from 'wagmi';
-import { Toaster } from 'react-hot-toast';
-import { useAutoConnectSafe, wagmiClient, chains } from '@/lib/wagmi';
-import PlausibleProvider from 'next-plausible';
+import { SessionProvider } from "next-auth/react";
+import { WagmiConfig } from "wagmi";
+import { Toaster } from "react-hot-toast";
+import { lightTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { useAutoConnectSafe, wagmiClient, chains } from "@/lib/wagmi";
+import PlausibleProvider from "next-plausible";
 
 const AutoConnectSafe = ({ children }) => {
-  useAutoConnectSafe()
-  return children
-}
+  useAutoConnectSafe();
+  return children;
+};
 
 function MyApp({ Component, pageProps }) {
   return (
     <PlausibleProvider domain="tokentable.org">
       <WagmiConfig client={wagmiClient}>
         <AutoConnectSafe>
-          <RainbowKitProvider chains={chains} theme={lightTheme({ accentColor: '#1455FE' })}>
-            <Component {...pageProps} />
-            <Toaster position="bottom-center" />
-          </RainbowKitProvider>
+          <SessionProvider refetchInterval={0} session={pageProps.session}>
+            <RainbowKitProvider
+              chains={chains}
+              theme={lightTheme({ accentColor: "#1455FE" })}
+            >
+              <Component {...pageProps} />
+              <Toaster position="bottom-center" />
+            </RainbowKitProvider>
+          </SessionProvider>
         </AutoConnectSafe>
       </WagmiConfig>
     </PlausibleProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
