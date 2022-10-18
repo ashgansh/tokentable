3ba55cfae032c40d8e6f7aed21452c946a6160e4
Laurent De Marez <laurent@demarez.org>
2022-10-18 19:58:38 +0200

    feat: record contract searches

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 import { PlusIcon, QueueListIcon } from "@heroicons/react/24/outline";
 
-import { formatCurrency, formatAmount } from "@/lib/utils";
+import { formatCurrency, formatAmount, record } from "@/lib/utils";
 import {
   findVestingContractChainId,
   getVestingContractDetails,
@@ -30,10 +30,7 @@ import { PrimaryButton } from "@/components/Button";
 
 export const NoPortfolioItems = () => {
   const exampleContracts = [
-    {
-      address: "0x2a7d59e327759acd5d11a8fb652bf4072d28ac04",
-    },
-    ,
+    { address: "0x2a7d59e327759acd5d11a8fb652bf4072d28ac04" },
   ];
 
   const {
@@ -49,6 +46,8 @@ export const NoPortfolioItems = () => {
   const handleAddContract = async (data) => {
     const chainId = await findVestingContractChainId(data.vestingContract);
     const isIndexed = !!chainId;
+
+    record(`Someone looked up a vesting contract. (${data.vestingContract}) (${isIndexed ? "supported" : "not supported"})`)
 
     if (isIndexed) {
       const details = await getVestingContractDetails(
@@ -70,8 +69,8 @@ export const NoPortfolioItems = () => {
 
   return (
     <div className="mx-auto max-w-lg">
-      <div className="text-center mb-2">
-        <QueueListIcon className="h-12 w-12 text-tokenops-primary-500 mx-auto" />
+      <div className="mb-2 text-center">
+        <QueueListIcon className="mx-auto h-12 w-12 text-tokenops-primary-500" />
         <h2 className="mt-2 text-lg font-medium text-gray-900">
           Track Vesting Schedules
         </h2>
@@ -97,14 +96,16 @@ export const NoPortfolioItems = () => {
             </span>
 
             <PrimaryButton
-            className="min-w-max ml-2"
-              type="submit" disabled={isSubmitting}>
-              {isSubmitting && <Spinner className="text-white h-4" />}
+              className="ml-2 min-w-max"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              {isSubmitting && <Spinner className="h-4 text-white" />}
               Track your vesting
             </PrimaryButton>
           </form>
           <div className="mt-10">
-            <h3 className="text-sm font-medium text-gray-500 text-left">
+            <h3 className="text-left text-sm font-medium text-gray-500">
               Try one of these examples. Just copy it in the search box.
             </h3>
             <ul
@@ -117,7 +118,7 @@ export const NoPortfolioItems = () => {
                   className="flex items-center justify-between space-x-3 py-2"
                 >
                   <div className="flex min-w-0 flex-1 items-center space-x-3">
-                    <div className="min-w-0 flex-1 min-h-[2rem] flex items-center">
+                    <div className="flex min-h-[2rem] min-w-0 flex-1 items-center">
                       <p className="truncate text-sm text-gray-500">
                         {contract.address}
                       </p>
@@ -139,7 +140,7 @@ export const NoPortfolioItems = () => {
         </div>
         {showIndexing && (
           <>
-            <div className="py-12 items-center flex flex-col gap-4">
+            <div className="flex flex-col items-center gap-4 py-12">
               <span>
                 {"We're indexing your contract. This may take up to a 24h."}
               </span>
