124cdb707be9123c4f2e378d1188b0168f48054b
Laurent De Marez <laurent@demarez.org>
2022-10-20 17:24:30 +0200

    chore: explore domain env var

--- pages/api/action.js
+++ pages/api/action.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 const { TELEGRAM_BOT_KEY, TELEGRAM_GROUP_ID, RECORD_ACTIONS } = process.env;
 const TELEGRAM_API_SEND_MSG_URL = `https://api.telegram.org/bot${TELEGRAM_BOT_KEY}/sendMessage`;
+const SHOULD_RECORD_ACTIONS = (process.env.VERCEL_URL = "tokentable.org");
 
 export default function handler(req, res) {
+  console.log(process.env.VERCEL_URL, SHOULD_RECORD_ACTIONS);
+
   if (RECORD_ACTIONS !== "true") {
     res.status(200).json({});
     return;
