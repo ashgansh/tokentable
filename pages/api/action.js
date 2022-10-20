38029eb354a53d30f0e4e1c186e48b82965d1cfe
Laurent De Marez <laurent@demarez.org>
2022-10-20 17:28:14 +0200

    fix: env var

--- pages/api/action.js
+++ pages/api/action.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const { TELEGRAM_BOT_KEY, TELEGRAM_GROUP_ID, RECORD_ACTIONS } = process.env;
 const TELEGRAM_API_SEND_MSG_URL = `https://api.telegram.org/bot${TELEGRAM_BOT_KEY}/sendMessage`;
-const SHOULD_RECORD_ACTIONS = (process.env.VERCEL_URL = "tokentable.org");
+const SHOULD_RECORD_ACTIONS = process.env.VERCEL_URL === "tokentable.org";
 
 export default function handler(req, res) {
   console.log(process.env.VERCEL_URL, SHOULD_RECORD_ACTIONS);
